fc8f978d46ad89e13dc91c9d647319400e830d81
WeixuanX <qb21029@bristol.ac.uk>
2021-10-11 19:58:22 +0100

    Create homework-week1-final.R

---
+++ homework-week1-final.R
@@ -0,0 +1,65 @@
+1+1
+2+2
+##add 2 and 2 together
+2+2
+ a<-1
+a
+c("a", 2)
+a <- c(1, 5, 4, 2)
+a
+g <- c(10, 2.1, 3)
+g
+c(a , g)
+c
+a <- c(1, 5, 4, 2)
+g <- c(10, 2.1, 3)
+c(a , g)
+a <- c(1, 2, 3, 4)
+a
+b <- c(5, 6, 7, 8)
+b
+c(a, b)
+a <- c(1, 2, 3, 4)
+a
+b <- c(5, 6, 7, 8)
+b
+c(a, b)
+d <- c(9, 10, a, 3)
+d
+d[3]
+d[2, 3]
+d[c(2,3)]
+d[c(2, 3)]
+a <- c(1, 5, 4, 2)
+a
+b <- a*4
+b
+a * b
+my_vec <- c("a" = 1, "d" = 3, "b" = 2)
+my_vec
+my_vec <- c(1, 3,2)
+names(my_vec) <- ("a", "b", "c")
+my_vec
+my_vec[2]
+sorted_vec <- sort(my_vec)
+sorted_vec
+sort(my_vec,decreasing = T)
+sort(my_vec)
+class_names <- c("James", "John", "Mary", "Linda", "Susan", "Mary", "Charles", "John", "James")
+class_names <- as.factor(class_names)
+x <- runif(n = 10, min =0, max =20)
+x
+x > 5
+x <- c(1:5) == 2
+x
+test <- c(1:10)
+test  <= 3 
+text >3|<2
+test > 3 | test < 2
+1:10 %in% c(1,3,5,9)
+test %in% c(1,3,5,9)
+sum_a <- sum(a)
+lenth_a <- length(a)
+sum_a/lenth_a
+1+1
+x
