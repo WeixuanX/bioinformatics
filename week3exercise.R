f74cb6e270d97e80175f11e3bb6437856aee98fb
WeixuanX <qb21029@bristol.ac.uk>
2021-11-05 18:00:22 +0000

    week3 homework and exercise

--- week3exercise.R
+++ week3exercise.R
@@ -93,8 +93,103 @@ install.packages("jsonlite", type = "source")
 
 ##make sure vroom is loaded into your R session (you only need to do this once)
 library(vroom)
-##???????????read in the wader data set
-wad_dat <- vroom("C:\Users\qb21029\OneDrive - University of Bristol\Desktop\wader_data.csv")
+##read in the wader data set
+wad_dat <- vroom("C:/Users/qb21029/OneDrive - University of Bristol/Desktop/wader_data.csv")
+##look at the top of the data
+head(wad_dat)
 
-##use vroom to read in some data from github:
-covid_dat <- vroom(https:/"github.com/WeixuanX/bioinformatics/blob/main/Data/Workshop%203/time_series_covid19_deaths_global.csv)
+##Relative pathways
+##first we set the working directory (which is the location of the current file you are working on):
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+##then read in the data using vroom:
+wad_dat <- vroom('./Data/wader_data.csv')
+
+
+##Task
+##first we set the working directory (which is the location of the current file you are working on):
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+##then read in the data using vroom:
+Covid_data <- vroom('./Data/time_series_covid19_deaths_global.csv')
+
+##4.1.2use vroom to read in some data from github:
+covid_dat <- vroom("https://raw.githubusercontent.com/chrit88/Bioinformatics_data/master/Workshop%203/time_series_covid19_deaths_global.csv")
+##4.1.2
+##you can ignore this code for the moment if you want
+##but to briefly summarise it is reading in some data included in base R
+##and then splitting it into 3 differnt data.frame style objects based on the values in one of the columns ("cyl")
+mt <- tibble::rownames_to_column(mtcars, "model")
+purrr::iwalk(
+  split(mt, mt$cyl),
+  ##save this split files in to the default directory
+  ~ vroom_write(.x, glue::glue("mtcars_{.y}.csv"), "\t")
+)
+
+##find files in the default directory which start with "mtcars" and end in "csv"
+##save these file names as an object called "files"
+files <- fs::dir_ls(glob = "mtcars*csv")
+
+##these are then the names of the files matching the above arguments:
+files
+##then load these file names using vroom 
+vroom(files)
+
+
+##4.2load in some RData
+load("my_data/pathway/my_data.RData")
+
+
+##6.1install the tidyverse
+install.packages("tidyverse")
+##load the tidyverse
+library("tidyverse")
+##6.2what class is the object
+class(covid_dat)
+##look at the data
+covid_dat
+##change the first two names of our data frame
+names(covid_dat)[1:2] <- c("Province.State", "Country.Region")
+##6.3so this says take our data frame called covid_dat
+covid_long <- covid_dat %>%
+  ##and then apply this function 
+  pivot_longer(cols = -c( Province.State, 
+                          Country.Region, 
+                          Lat, 
+                          Long))
+
+##our data frame
+covid_long <- covid_dat %>%
+  ##and then apply this function 
+  pivot_longer(cols = -c(Province.State:Long),
+               names_to = "Date",
+               values_to = "Deaths")
+
+##change long to wide
+covid_wide <- covid_long %>% 
+  pivot_wider(names_from = Date,
+              values_from = Deaths)
+
+
+
+
+
+
+##homework
+endanger1 <- vroom("C:/Users/qb21029/OneDrive - University of Bristol/Documents/GitHub/bioinformatics/Bioinformatics_data_to_sort_pop_1.csv")
+endanger2 <- vroom("C:/Users/qb21029/OneDrive - University of Bristol/Documents/GitHub/bioinformatics/Bioinformatics_data_to_sort_pop_2.csv")
+
+
+endanger_long <- endanger %>%
+  pivot_longer(cols = -c(species, 
+                         primary_threat, 
+                         secondary_threat, 
+                         tertiary_threat), 
+               names_to = c("population", "date"),
+               names_pattern = "(.*)_(.*)",
+               values_drop_na = F, 
+               values_to = "abundance")
+
+
+
+
